400728c2964d624e7b68c53bc63e63385bb34d1a
jh_future <1412881291@qq.com>
2023-07-09 23:18:31 +0800

    first commit

---
+++ kb.h
@@ -0,0 +1,20 @@
+#ifndef KB_H
+#define KB_H
+
+#include "quantum.h"
+
+#define KEYMAP(                    \
+	K00, K01, K02, K03,            \
+	K10, K11, K12, K13,            \
+	K20, K21, K22, K23,            \
+	K32, K33)                      \
+	{                              \
+		{K00, K01, K02, K03},      \
+			{K10, K11, K12, K13},  \
+			{K20, K21, K22, K23},  \
+		{                          \
+			KC_NO, KC_NO, K32, K33 \
+		}                          \
+	}
+
+#endif
